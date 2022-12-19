c78ebfe58c0a7b8486c177b4e7b7bdc14a27372a
manq2010 <manq2010@gmail.com>
2022-12-20 00:16:39 +0200

    fetch data from missions API

--- src/features/Mission/Mission.jsx
+++ src/features/Mission/Mission.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions } from './missionSlice';
 
 const Mission = () => {
-  const mission = useSelector((state) => state.missionReducer);
-  //   console.log(addMission);
-  console.log(mission);
+  const { isMember, missions, status } = useSelector((state) => state.missionReducer);
+
+  console.log(isMember);
+  console.log(missions);
+  console.log(status);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
 
   return (
     <div>
@@ -16,12 +25,18 @@ const Mission = () => {
           <h4>Status</h4>
           <></>
         </li>
-        <li>
-          <h4>Mission</h4>
-          <h4>Description</h4>
-          <h4>Status</h4>
-          <></>
-        </li>
+        {
+            missions.map((mission) => (
+              <li key={mission.mission_id}>
+                <h4>
+                  {mission.mission_name}
+                </h4>
+                <p>{mission.description}</p>
+                <button type="button">Not a Member</button>
+                <button type="button">Join Mission</button>
+              </li>
+            ))
+        }
       </ul>
 
     </div>
