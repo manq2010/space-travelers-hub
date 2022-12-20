dd651066a4b4ce758e453b3ff6324f96f1f4bc00
manq2010 <manq2010@gmail.com>
2022-12-20 13:00:09 +0200

    Add basic mission styling

--- src/features/Mission/Mission.jsx
+++ src/features/Mission/Mission.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 // Import membership reducer:
+import styled from 'styled-components';
 import { membership } from './missionSlice';
+
+const MissionWrapper = styled.div`
+
+`;
+
+const MissionList = styled.li`
+list-style: none;
+display: grid;
+grid-template-columns: 1fr 2fr 1fr 1fr;
+margin: 0;
+border: 1px solid gray;
+`;
+
+const Header = styled.h4`
+display: flex;
+justify-content: space-between;
+align-items: center;
+`;
+
+const MissionName = styled(Header)`
+
+`;
+
+const StatusButton = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+const StatusBtnContainer = styled(StatusButton)`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+const ButtonMember = styled.button`
+text-transform: uppercase;
+`;
+
+const ButtonJoin = styled.button`
+
+`;
+
+const MissionDescription = styled.p`
+display: flex;
+`;
 
 const Mission = () => {
   const { isMember, missions, status } = useSelector((state) => state.missionReducer);
@@ -23,30 +70,34 @@ const Mission = () => {
   //   }, [status, dispatch]);
 
   return (
-    <div>
+    <MissionWrapper>
 
       <ul>
-        <li>
-          <h4>Mission</h4>
-          <h4>Description</h4>
-          <h4>Status</h4>
-          <></>
-        </li>
+        <MissionList>
+          <MissionName>Mission</MissionName>
+          <Header>Description</Header>
+          <Header>Status</Header>
+          <Header />
+        </MissionList>
         {
             missions.map((mission) => (
-              <li key={mission.mission_id}>
-                <h4>
+              <MissionList key={mission.mission_id}>
+                <MissionName>
                   {mission.mission_name}
-                </h4>
-                <p>{mission.description.substring(0, 100)}</p>
-                <button type="button">Not a Member</button>
-                <button type="button">Join Mission</button>
-              </li>
+                </MissionName>
+                <MissionDescription>{mission.description.substring(0, 200)}</MissionDescription>
+                <StatusBtnContainer>
+                  <ButtonMember type="button">Not a Member</ButtonMember>
+                </StatusBtnContainer>
+                <StatusBtnContainer>
+                  <ButtonJoin type="button">Join Mission</ButtonJoin>
+                </StatusBtnContainer>
+              </MissionList>
             ))
         }
       </ul>
 
-    </div>
+    </MissionWrapper>
   );
 };
 
