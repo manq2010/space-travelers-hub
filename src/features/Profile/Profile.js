162c4323ecb6f359504ebe350491970a17fa2f38
manq2010 <manq2010@gmail.com>
2022-12-22 07:09:20 +0200

    Add leave-mission button for dispatching leave missission action

--- src/features/Profile/Profile.js
+++ src/features/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import { leaveMission } from '../Mission/missionSlice';
 
 const ProfileContainer = styled.div`
 display: flex;
@@ -20,10 +21,18 @@ border 1px solid gray;
 const JoinedItem = styled.div`
 border-bottom: 1px solid gray;
 padding: 1rem;
+display: flex;
+justify-content: space-between;
+gap: 0.5rem;
+`;
+const ButtonLeaveMission = styled.button`
+
 `;
 
 const Profile = () => {
   const missions = useSelector((state) => state.missionReducer.missions);
+
+  const dispatch = useDispatch();
 
   const joinedMission = missions.filter((mission) => mission.reserved === true);
 
@@ -38,14 +47,26 @@ const Profile = () => {
       <ProfileCard>
         <h2>My Missions</h2>
         {
-          missionContent
-            ? (<h3>no missions joined</h3>) : ('')
+          missionContent ? (<h3>no missions joined</h3>) : ('')
         }
         <ProfileCardDetails>
           {
             joinedMission.map((mission) => (
               <JoinedItem key={mission.mission_id}>
-                {mission.mission_name}
+                <div>
+                  {mission.mission_name}
+                </div>
+                <div>
+                  <ButtonLeaveMission
+                    type="button"
+                    onClick={() => {
+                      dispatch(leaveMission(mission.mission_id));
+                    }}
+                  >
+                    Leave Mission
+
+                  </ButtonLeaveMission>
+                </div>
               </JoinedItem>
             ))
         }
